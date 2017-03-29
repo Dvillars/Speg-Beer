53348372a66a23b1cdcf4a4eae4853e9dac69960
Derek Villars and Lawrence Eby <derek+lawrence@>
2017-03-29 11:31:41 -0700

    Add form

--- app/new-speg-beer.component.ts
+++ app/new-speg-beer.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import{ Keg } from './objects/keg';
 
 @Component({
   selector: 'new-speg',
-  template: ``
+  templateUrl: './../app/views/new-speg.html'
 })
 
 export class NewSpegComponent {
-
+  @Output() sendNewObject = new EventEmitter();
 }
