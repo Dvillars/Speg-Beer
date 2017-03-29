c02e1900dddf1b27b62e1800e57e860eedfa1823
Derek Villars and Lawrence Eby <derek+lawrence@>
2017-03-29 14:02:13 -0700

    Add form fixed position to help with page flow and testing

--- app/keg-update.component.ts
+++ app/keg-update.component.ts
@@ -9,6 +9,42 @@ import { Keg } from './objects/keg';
 export class UpdateComponent{
   @Input() keg: Keg;
   servePint(keg) {
-    keg.pints-= 1;
+    if(keg.pints === "You are out of this") {
+      alert("You cannot sell pints you are out of!");
+    } else {
+      keg.pints-= 1;
+      if(keg.pints === 0)
+      {
+        keg.pints = "You are out of this";
+      }
+    }
+  }
+
+  serveGrowler(keg) {
+    if(keg.pints === "You are out of this") {
+      alert("You cannot sell pints you are out of!");
+    } else if(keg.pints < 2){
+      alert("You do no have enough for a small growler");
+    } else {
+      keg.pints-= 2;
+      if(keg.pints === 0)
+      {
+        keg.pints = "You are out of this";
+      }
+    }
+  }
+
+  serveLargeGrowler(keg) {
+    if(keg.pints === "You are out of this") {
+      alert("You cannot sell pints you are out of!");
+    } else if(keg.pints < 4){
+      alert("You do no have enough for a small growler");
+    } else {
+      keg.pints-= 4;
+      if(keg.pints === 0)
+      {
+        keg.pints = "You are out of this";
+      }
+    }
   }
 }
